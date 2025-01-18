0627069bf3b49befb72436e97e6cffe6697586a4
aminhasanz <hasanzamin80@gmail.com>
2025-01-18 14:09:04 +0330

    adding dapper

---
+++ api/Library/Model/Table/BookTable.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Model.Table
+{
+    public class BookTable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Pages { get; set; }
+    }
+}
---
+++ api/Library/Program.cs
@@ -0,0 +1,14 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=.;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            SqlConnection connection = new SqlConnection(connectionString);
+        }
+    }
+}
